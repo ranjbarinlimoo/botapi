1427cf405088dcb9dba005850da08736e2c7d032
MohammadReza Ranjbar <ranjbar@parsdata.com>
2021-08-26 14:46:09 +0430

    cv

--- main.js
+++ main.js
@@ -1,29 +1,20 @@
 const TelegramBot = require('node-telegram-bot-api');
-const request = require('request')
 const express = require('express');
-const fs = require('fs')
-const token = '1718858345:AAHDENdqpfXfoOnt7aEIhXmtUqgs0j6ZIMA';
-const bot = new TelegramBot(token, {polling: true});
-console.log('Created!!!!')
-const port = process.env.PORT || 3000
-const app = express()
-app.listen(port)
-let lastId = 70607680
 
-bot.onText(/(.+)/, (msg, match) => {
+const app = express()
+const bot = new TelegramBot(token, {polling: true});
+const port = process.env.PORT || 3000
+
+const token = '1718858345:AAHDENdqpfXfoOnt7aEIhXmtUqgs0j6ZIMA';
+console.log('Running On Port: '+port)
+app.listen(port)
+
+bot.onText(/(.+)/, async (msg, match) => {
 
 
     const chatId = msg.chat.id;
-    lastId = chatId
     const resp = match[1];
 
-    bot.sendMessage(chatId,port)
+    await bot.sendMessage(chatId,resp)
 
 });
-
-app.get('/',async (req,res)=>{
-
-    await bot.sendMessage(lastId, lastId)
-    res.send('OK')
-
-})
