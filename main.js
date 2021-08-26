adbc37f749cb75a6d89c418aa7a82948143d87c5
MohammadReza Ranjbar <ranjbar@parsdata.com>
2021-08-26 14:51:20 +0430

    cv

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
 
+const token = '1718858345:AAHDENdqpfXfoOnt7aEIhXmtUqgs0j6ZIMA';
 const app = express()
 const bot = new TelegramBot(token, {polling: true});
-const port = process.env.PORT || 3000
 
-const token = '1718858345:AAHDENdqpfXfoOnt7aEIhXmtUqgs0j6ZIMA';
+const port = process.env.PORT || 3000
 console.log('Running On Port: '+port)
 app.listen(port)
 
